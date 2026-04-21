acf88e3d4331123cbccaf5747a90446410c7239f
Prashan5903s <chaubeyprashant498@gmail.com>
2026-04-21 12:43:10 +0530

    Changes of user dashboard

--- src/components/dialogs/follow_up_table_dialog/page.jsx
+++ src/components/dialogs/follow_up_table_dialog/page.jsx
@@ -148,7 +148,7 @@ const FollowUpTable = ({ open, setOpen, tableData, fetchRoleData, selectedLeadId
 
         const limitedText = notes
           .split(" ")
-          .slice(0, 20)
+          .slice(0, 5)
           .join(" ");
 
         return (
@@ -156,7 +156,7 @@ const FollowUpTable = ({ open, setOpen, tableData, fetchRoleData, selectedLeadId
 
             {limitedText}
 
-            {notes.split(" ").length > 20 ? "..." : ""}
+            {notes.split(" ").length > 5 ? "..." : ""}
           </Typography>
         );
       }
